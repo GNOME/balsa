e2dd6fd6881dd8bc302f946a1c33d58211b590d3
Stuart Parmenter <pavlov@src.gnome.org>
1998-03-08 22:38:54 +0000

    gnomified balsa (not finished) -pav

--- src/main-window.c
+++ src/main-window.c
@@ -1,5 +1,5 @@
 /* Balsa E-Mail Client
- * Copyright (C) 1997-98 Jay Painter
+ * Copyright (C) 1997-98 Jay Painter and Stuart Parmenter
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,40 +32,200 @@ void show_about_box (GtkWidget * widget, gpointer data);
 /* from main.c -- exit balsa */
 extern void balsa_exit ();
 
-/* menu structure for menu factory */
-static GtkMenuEntry main_menu_items[] =
+static GtkWidget *menu_items[18];
+
+static GtkWidget *
+create_menu (GtkWidget * window)
 {
-  {"<Balsa>/File/Get New Mail", "<control>M", current_mailbox_check, NULL},
-  {"<Balsa>/File/<separator>", NULL, NULL, NULL},
-  {"<Balsa>/File/New Folder...", NULL, NULL, NULL},
-  {"<Balsa>/File/Open Folder...", NULL, NULL, NULL},
-  {"<Balsa>/File/Save Folder", NULL, NULL, NULL},
-  {"<Balsa>/File/<separator>", NULL, NULL, NULL},
-  {"<Balsa>/File/Print", NULL, NULL, NULL},
-  {"<Balsa>/File/<separator>", NULL, NULL, NULL},
-  {"<Balsa>/File/Exit", "<control>Q", balsa_exit, NULL},
-  {"<Balsa>/Edit/Copy", "<control>C", NULL, "Copy"},
-  {"<Balsa>/Edit/Cut", "<control>X", NULL, "Cut"},
-  {"<Balsa>/Edit/Paste", "<control>V", NULL, "Paste"},
-  {"<Balsa>/Message/New", "<control>N", sendmsg_window_new, NULL},
-  {"<Balsa>/Message/Reply", "<control>R", NULL, NULL},
-  {"<Balsa>/Message/Reply to All", NULL, NULL, NULL},
-  {"<Balsa>/Message/Foward", NULL, NULL, NULL},
-  {"<Balsa>/Message/Redirect", NULL, NULL, NULL},
-  {"<Balsa>/Message/Send Again", NULL, NULL, NULL},
-  {"<Balsa>/Message/<separator>", NULL, NULL, NULL},
-/*
-   This needs to be part of the menu on the new_message window, not the main one
-   -pav
-   { "<Balsa>/Message/Attach File to New Message", "<control>H", NULL, NULL },
- */
-  {"<Balsa>/Message/Delete", "<control>D", NULL, NULL},
-  {"<Balsa>/Tools/Personalities...", NULL, personality_box, NULL},
-  {"<Balsa>/Tools/Settings...", NULL, NULL, NULL},
-  {"<Balsa>/Help/Contents", NULL, NULL, NULL},
-  {"<Balsa>/Help/About", NULL, show_about_box, NULL}
-};
-static int main_nmenu_items = sizeof (main_menu_items) / sizeof (main_menu_items[0]);
+  GtkWidget *menubar, *w, *menu;
+  GtkAcceleratorTable *accel;
+  int i = 0;
+
+  accel = gtk_accelerator_table_new ();
+  menubar = gtk_menu_bar_new ();
+  gtk_widget_show (menubar);
+
+  menu = gtk_menu_new ();
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Get New Mail"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'M', GDK_CONTROL_MASK);
+  gtk_signal_connect_object (GTK_OBJECT (w), "activate",
+			     (GtkSignalFunc) current_mailbox_check,
+			     GTK_OBJECT (window));
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new ();
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("New mailbox..."));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Save mailbox..."));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new ();
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Print..."));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new ();
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_EXIT, _ ("Exit"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'Q', GDK_CONTROL_MASK);
+  gtk_signal_connect_object (GTK_OBJECT (w), "activate",
+			     (GtkSignalFunc) gtk_widget_destroy,
+			     GTK_OBJECT (window));
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new_with_label (_ ("File"));
+  gtk_widget_show (w);
+  gtk_menu_item_set_submenu (GTK_MENU_ITEM (w), menu);
+  gtk_menu_bar_append (GTK_MENU_BAR (menubar), w);
+
+  menu = gtk_menu_new ();
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_CUT, _ ("Cut"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'X', GDK_CONTROL_MASK);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_COPY, _ ("Copy"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'C', GDK_CONTROL_MASK);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_PASTE, _ ("Paste"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'V', GDK_CONTROL_MASK);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new_with_label (_ ("Edit"));
+  gtk_widget_show (w);
+  gtk_menu_item_set_submenu (GTK_MENU_ITEM (w), menu);
+  gtk_menu_item_right_justify (GTK_MENU_ITEM (w));
+  gtk_menu_bar_append (GTK_MENU_BAR (menubar), w);
+
+  menu = gtk_menu_new ();
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("New"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'N', GDK_CONTROL_MASK);
+  gtk_signal_connect_object (GTK_OBJECT (w), "activate",
+			     (GtkSignalFunc) sendmsg_window_new,
+			     NULL);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Reply"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'R', GDK_CONTROL_MASK);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Reply to All"));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Foward"));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Redirect"));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Send again"));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new ();
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Delete"));
+  gtk_widget_show (w);
+  gtk_widget_install_accelerator (w, accel, "activate",
+				  'D', GDK_CONTROL_MASK);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new_with_label (_ ("Message"));
+  gtk_widget_show (w);
+  gtk_menu_item_set_submenu (GTK_MENU_ITEM (w), menu);
+  gtk_menu_item_right_justify (GTK_MENU_ITEM (w));
+  gtk_menu_bar_append (GTK_MENU_BAR (menubar), w);
+
+  menu = gtk_menu_new ();
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Account manager"));
+  gtk_widget_show (w);
+  gtk_signal_connect_object (GTK_OBJECT (w), "activate",
+			     (GtkSignalFunc) personality_box,
+			     NULL);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_BLANK, _ ("Settings"));
+  gtk_widget_show (w);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new_with_label (_ ("Tools"));
+  gtk_widget_show (w);
+  gtk_menu_item_set_submenu (GTK_MENU_ITEM (w), menu);
+  gtk_menu_item_right_justify (GTK_MENU_ITEM (w));
+  gtk_menu_bar_append (GTK_MENU_BAR (menubar), w);
+
+  menu = gtk_menu_new ();
+
+  w = gnome_stock_menu_item (GNOME_STOCK_MENU_ABOUT, _ ("About"));
+  gtk_widget_show (w);
+  gtk_signal_connect_object (GTK_OBJECT (w), "activate",
+			     (GtkSignalFunc) show_about_box,
+			     NULL);
+  gtk_menu_append (GTK_MENU (menu), w);
+  menu_items[i++] = w;
+
+  w = gtk_menu_item_new_with_label (_ ("Help"));
+  gtk_widget_show (w);
+  gtk_menu_item_set_submenu (GTK_MENU_ITEM (w), menu);
+  gtk_menu_item_right_justify (GTK_MENU_ITEM (w));
+  gtk_menu_bar_append (GTK_MENU_BAR (menubar), w);
+
+  menu_items[i] = NULL;
+/*      g_print("%d menu items\n", i); */
+  gtk_window_add_accelerator_table (GTK_WINDOW (window), accel);
+  return menubar;
+}
 
 
 MainWindow *
@@ -84,9 +244,9 @@ create_main_window ()
   mw->move_menu = NULL;
 
   /* main window */
-  mw->window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title (GTK_WINDOW (mw->window), "Balsa");
-  gtk_container_border_width (GTK_CONTAINER (mw->window), 0);
+  mw->window = gnome_app_new ("balsa", "Balsa");
+  gtk_window_set_wmclass (GTK_WINDOW (mw->window), "balsa_app",
+			  "Balsa");
 
   gtk_signal_connect (GTK_OBJECT (mw->window),
 		      "destroy",
@@ -100,18 +260,7 @@ create_main_window ()
 
 
   vbox = gtk_vbox_new (FALSE, 0);
-  gtk_container_add (GTK_CONTAINER (mw->window), vbox);
   gtk_widget_show (vbox);
-
-
-  /* set menu bar */
-  mw->factory = gtk_menu_factory_new (GTK_MENU_FACTORY_MENU_BAR);
-  mw->subfactories[0] = gtk_menu_factory_new (GTK_MENU_FACTORY_MENU_BAR);
-  gtk_menu_factory_add_subfactory (mw->factory, mw->subfactories[0], "<Balsa>");
-  gtk_menu_factory_add_entries (mw->factory, main_menu_items, main_nmenu_items);
-  mw->menubar = mw->subfactories[0]->widget;
-  gtk_box_pack_start (GTK_BOX (vbox), mw->menubar, FALSE, TRUE, 0);
-  gtk_widget_show (mw->menubar);
 
 
   /* toolbar */
@@ -137,11 +286,11 @@ create_main_window ()
 
   gtk_toolbar_append_space (GTK_TOOLBAR (mw->toolbar));
 
-  gtk_toolbar_append_item (GTK_TOOLBAR (mw->toolbar), "Compose", 
+  gtk_toolbar_append_item (GTK_TOOLBAR (mw->toolbar), "Compose",
 			   NULL, NULL, NULL, NULL, NULL);
-  gtk_toolbar_append_item (GTK_TOOLBAR (mw->toolbar), "Reply", 
+  gtk_toolbar_append_item (GTK_TOOLBAR (mw->toolbar), "Reply",
 			   NULL, NULL, NULL, NULL, NULL);
-  gtk_toolbar_append_item (GTK_TOOLBAR (mw->toolbar), "Delete", 
+  gtk_toolbar_append_item (GTK_TOOLBAR (mw->toolbar), "Delete",
 			   NULL, NULL, NULL, NULL, NULL);
 
   gtk_widget_show (mw->toolbar);
@@ -160,9 +309,9 @@ create_main_window ()
   gtk_box_pack_start (GTK_BOX (hbox), mw->index, TRUE, TRUE, 2);
 
   gtk_signal_connect (GTK_OBJECT (mw->index),
-                      "select_message",
-                      (GtkSignalFunc) index_select,
-                      NULL);
+		      "select_message",
+		      (GtkSignalFunc) index_select,
+		      NULL);
 
   gtk_paned_add1 (GTK_PANED (vpane), hbox);
   gtk_widget_show (hbox);
@@ -180,6 +329,15 @@ create_main_window ()
   gtk_paned_add2 (GTK_PANED (vpane), hbox);
   gtk_widget_show (hbox);
 
+  gnome_app_set_contents (GNOME_APP (mw->window), vbox);
+
+  gnome_app_set_menus (GNOME_APP (mw->window),
+		       GTK_MENU_BAR (create_menu (mw->window)));
+/*
+   gnome_app_set_toolbar(GNOME_APP(mw->window),
+   GTK_TOOLBAR(create_toolbar(mw->window)));
+
+ */
   gtk_widget_show (mw->window);
   return mw;
 }
@@ -199,6 +357,6 @@ show_about_box (GtkWidget * widget, gpointer data)
 				      "Copyright (C) 1997-98",
 				      authors,
 				      "The flimsey e-mail client!",
-				      "/tmp/balsa.xpm");
+				      "balsa.xpm");
   gtk_widget_show (about);
 }

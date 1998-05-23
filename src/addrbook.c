ba4694cbcb3b8c15932a0f68bab0e4ed90027b4f
Stuart Parmenter <pavlov@src.gnome.org>
1998-05-23 18:45:42 +0000

    mooooooooore stuff -pav
    mooooooooore stuff
    -pav

--- src/addrbook.c
+++ src/addrbook.c
@@ -11,16 +11,64 @@
 AddressBook *
 addressbook_read_pine (gchar * file)
 {
-  int fd, ret;
+  int fd, ret, i, j=0;
+
+  int ine = 0, tabnum = 0;
+
   char buff[4096];
+  char tmp[4096];
   fd = open (file, O_RDONLY);
   if (fd == -1)
     {
-      perror ("error opening signature file");
+      perror ("error opening address book file");
       return FALSE;
     }
   ret = read (fd, buff, 4096);
-/*if (ret > 0) */
-  printf ("%s\n", buff);
+  if (ret > 0)
+    buff[ret - 1] = '\0';
+  g_print ("%s\n", buff);
+
+  for (i = 0; i < ret; i++)
+    {
+      if (buff[i] == '\r' && buff[i + 1] == '\n')
+	{
+	  tabnum = ine = 0;
+	  i++;
+	}
+      else if (buff[i] == '\n' && buff[i + 1] == '\r')
+	{
+	  tabnum = ine = 0;
+	  i++;
+	}
+      else if (buff[i] == '\n')
+	{
+	  tabnum = ine = 0;
+	}
+      else if (buff[i] == '\r')
+	{
+	  tabnum = ine = 0;
+	}
+
+      else switch (buff[i])
+	{
+	case '\t':
+	  g_print("%i: %s\n",tabnum, tmp);
+	  *tmp = '\0';
+	  j=0;
+	  tabnum++;
+	  break;
+	case '(':
+	  ine = 1;
+	  break;
+	case ')':
+	  ine = 0;
+	  break;
+	default:
+	  tmp[j] = buff[i];
+	  j++;
+	  break;
+	}
+    }
+
   close (fd);
 }

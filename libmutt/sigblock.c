6ea8b298c3ba9fa300f69837339b15150ff42c4e
Alan Cox <alan@src.gnome.org>
1998-07-16 19:10:08 +0000

    sigblock needs to own the Sigset-t object as its the one that may be linked alone

--- libmutt/sigblock.c
+++ libmutt/sigblock.c
@@ -23,7 +23,7 @@
 #include <string.h>
 #include <sys/wait.h>
 
-extern sigset_t Sigset;
+sigset_t Sigset;
 
 /* signals which are important to block while doing critical ops */
 void mutt_block_signals (void)

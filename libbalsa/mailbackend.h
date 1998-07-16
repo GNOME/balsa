a84d6651e6ef756e7e7e0dda424229ad47f4808a
Alan Cox <alan@src.gnome.org>
1998-07-16 15:59:59 +0000

    Added libmutt/ to the includes otherwise the wrong mailbox.h is included libbalse now almost compiles

--- libbalsa/mailbackend.h
+++ libbalsa/mailbackend.h
@@ -1,9 +1,9 @@
 #ifndef __LIB_MUTT_HEADERS__
 #define __LIB_MUTT_HEADERS__
-#include "mutt.h"
-#include "mx.h"
-#include "rfc2047.h"
-#include "sort.h"
-#include "mailbox.h"
-#include "copy.h"
+#include "libmutt/mutt.h"
+#include "libmutt/mx.h"
+#include "libmutt/rfc2047.h"
+#include "libmutt/sort.h"
+#include "libmutt/mailbox.h"
+#include "libmutt/copy.h"
 #endif

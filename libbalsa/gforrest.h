eb9c8b3927102fccb4701d9fbf7d57333db28186
Stuart Parmenter <pavlov@src.gnome.org>
1998-07-28 02:37:10 +0000

    whoops, I forgot to add gforrest.[ch].
    This currently doesn't do anything useful, but will be used to store things in
    a heirarchy.  Since GTree is already being used, i figured we'd just use
    forrest (;
    
    Once it does something useful, we'll probibly add it to glib.  If someone
    wants to do this for me, or already has, please speak up :)
    
    Stuart Parmenter <pavlov@pavlov.net>

---
+++ libbalsa/gforrest.h
@@ -0,0 +1,28 @@
+/* Balsa E-Mail Library
+ * Copyright (C) 1998 Stuart Parmenter
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+typedef struct _GForrest GForrest;
+struct _GForrest
+  {
+    gchar *key;
+    gpointer data;
+    GList *list;
+    GList *sibling;
+    GList *children;
+  };

03c6da4f929c1906d5741f4129291ca1bbf33649
Stuart Parmenter <pavlov@src.gnome.org>
1998-12-08 00:18:36 +0000

    use GnomeUIInfo for the toolbar

--- src/main-window.c
+++ src/main-window.c
@@ -47,10 +47,8 @@ static gint about_box_visible = FALSE;
 
 /* main window widget components */
 static gint progress_timeout (gpointer data);
-static void create_menu (GnomeMDI *);
 
 static void app_created (GnomeMDI *, GnomeApp * app);
-static GtkToolbar *create_toolbar (GnomeMDI *, GnomeApp * app);
 
 
 /* dialogs */
@@ -81,6 +79,107 @@ static void about_box_destroy_cb (void);
 static void destroy_window_cb (GnomeMDI * mdi, gpointer data);
 
 static void set_icon (GdkWindow * window);
+
+static GnomeUIInfo file_menu[] =
+{
+    /* Ctrl-M */
+  {
+ GNOME_APP_UI_ITEM, N_ ("_Get new mail"), NULL, check_new_messages_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_RCV, 'M', GDK_CONTROL_MASK, NULL
+  },
+  GNOMEUIINFO_SEPARATOR,
+  {
+    GNOME_APP_UI_ITEM, N_ ("E_xit"), NULL, close_main_window, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_EXIT, 'Q', 0, NULL
+  },
+  GNOMEUIINFO_END
+};
+
+static GnomeUIInfo message_menu[] =
+{
+    /* M */
+  {
+    GNOME_APP_UI_ITEM, N_ ("_New"), NULL, new_message_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL, 'M', 0, NULL
+  },
+    /* R */
+  {
+    GNOME_APP_UI_ITEM, N_ ("_Reply"), NULL, replyto_message_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_RPL, 'R', 0, NULL
+  },
+    /* A */
+  {
+ GNOME_APP_UI_ITEM, N_ ("Reply to _all"), NULL, replytoall_message_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_RPL, 'A', 0, NULL
+  },
+    /* F */
+  {
+    GNOME_APP_UI_ITEM, N_ ("_Forward"), NULL, forward_message_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_FWD, 'F', 0, NULL
+  },
+  GNOMEUIINFO_SEPARATOR,
+    /* D */
+  {
+    GNOME_APP_UI_ITEM, N_ ("_Delete"), NULL, delete_message_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_TRASH, 'D', 0, NULL
+  },
+    /* U */
+  {
+    GNOME_APP_UI_ITEM, N_ ("_Undelete"), NULL, undelete_message_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_UNDELETE, 'U', 0, NULL
+  },
+  GNOMEUIINFO_END
+};
+static GnomeUIInfo mailbox_menu[] =
+{
+    /* C */
+  {
+    GNOME_APP_UI_ITEM, N_ ("List"), NULL, mblist_window_cb, NULL,
+    NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_PROP, 'C', 0, NULL
+  },
+  GNOMEUIINFO_ITEM_STOCK ("Close", NULL, mailbox_close_child, GNOME_STOCK_MENU_CLOSE),
+  GNOMEUIINFO_SEPARATOR,
+  GNOMEUIINFO_END
+};
+static GnomeUIInfo settings_menu[] =
+{
+  GNOMEUIINFO_ITEM_STOCK ("_Filters...", NULL, filter_dlg_cb, GNOME_STOCK_MENU_PROP),
+  GNOMEUIINFO_ITEM_STOCK ("_Preferences...", NULL, open_preferences_manager, GNOME_STOCK_MENU_PROP),
+  GNOMEUIINFO_END
+};
+static GnomeUIInfo help_menu[] =
+{
+  GNOMEUIINFO_ITEM_STOCK ("_About...", NULL, show_about_box, GNOME_STOCK_MENU_ABOUT),
+  GNOMEUIINFO_HELP ("balsa"),
+  GNOMEUIINFO_END
+};
+static GnomeUIInfo main_menu[] =
+{
+  GNOMEUIINFO_SUBTREE ("_File", file_menu),
+  GNOMEUIINFO_SUBTREE ("_Message", message_menu),
+  GNOMEUIINFO_SUBTREE ("Mail_boxes", mailbox_menu),
+  GNOMEUIINFO_SUBTREE ("_Settings", settings_menu),
+  GNOMEUIINFO_SUBTREE ("_Help", help_menu),
+  GNOMEUIINFO_END
+};
+
+static GnomeUIInfo main_toolbar[] =
+{
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Check"), N_ ("Check Email"), check_new_messages_cb, GNOME_STOCK_PIXMAP_MAIL_RCV),
+  GNOMEUIINFO_SEPARATOR,
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Delete"), N_ ("Delete Message"), delete_message_cb, GNOME_STOCK_PIXMAP_TRASH),
+  GNOMEUIINFO_SEPARATOR,
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Compose"), N_ ("Compose Message"), new_message_cb, GNOME_STOCK_PIXMAP_MAIL_NEW),
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Reply"), N_ ("Reply"), replyto_message_cb, GNOME_STOCK_PIXMAP_MAIL_RPL), 
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Reply to all"), N_ ("Reply to all"), replytoall_message_cb, GNOME_STOCK_PIXMAP_MAIL_RPL),
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Forward"), N_ ("Forward"), forward_message_cb, GNOME_STOCK_PIXMAP_MAIL_FWD),
+  GNOMEUIINFO_SEPARATOR,
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Previous"), N_ ("Open Previous Message"), previous_message_cb, GNOME_STOCK_PIXMAP_BACK),
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Next"), N_ ("Open Next Message"), next_message_cb, GNOME_STOCK_PIXMAP_FORWARD),
+  GNOMEUIINFO_SEPARATOR,
+  GNOMEUIINFO_ITEM_STOCK (N_ ("Print"), N_ ("Print current message"), NULL, GNOME_STOCK_PIXMAP_PRINT),
+  GNOMEUIINFO_END
+};
 
 void
 main_window_set_cursor (gint type)
@@ -137,14 +236,14 @@ main_window_init (void)
 
   /* meubar and toolbar */
   gtk_signal_connect (GTK_OBJECT (mdi), "child_changed", GTK_SIGNAL_FUNC (index_child_changed), NULL);
-  gtk_signal_connect (GTK_OBJECT (mdi), "app_created",GTK_SIGNAL_FUNC (app_created), NULL);
+  gtk_signal_connect (GTK_OBJECT (mdi), "app_created", GTK_SIGNAL_FUNC (app_created), NULL);
   gnome_mdi_set_child_list_path (mdi, _ ("Mailboxes/<Separator>"));
 
-  create_menu(mdi);
-  gnome_mdi_set_toolbar_creator(mdi, create_toolbar);
-  
+  gnome_mdi_set_menubar_template (mdi, main_menu);
+  gnome_mdi_set_toolbar_template (mdi, main_toolbar);
+
   gnome_mdi_set_mode (mdi, balsa_app.mdi_style);
-  gnome_mdi_open_toplevel(mdi);
+  gnome_mdi_open_toplevel (mdi);
 }
 
 static gint
@@ -189,7 +288,7 @@ app_created (GnomeMDI * mdi, GnomeApp * app)
   gtk_widget_set_usize (GTK_WIDGET (app), balsa_app.mw_width, balsa_app.mw_height);
 
 
-  refresh_main_window();
+  refresh_main_window ();
 }
 
 /*
@@ -217,206 +316,6 @@ refresh_main_window (void)
   gtk_toolbar_set_style (GTK_TOOLBAR (GNOME_APP (mdi->active_window)->toolbar), balsa_app.toolbar_style);
   gnome_mdi_set_mode (mdi, balsa_app.mdi_style);
 }
-
-/*
- * the menubar for the main window
- */
-
-static void
-create_menu (GnomeMDI * mdi)
-{
-  static GnomeUIInfo file_menu[] = {
-    /* Ctrl-M */
-    {GNOME_APP_UI_ITEM, N_("_Get new mail"), NULL, check_new_messages_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_RCV, 'M', GDK_CONTROL_MASK, NULL},
-    GNOMEUIINFO_SEPARATOR,
-    {GNOME_APP_UI_ITEM, N_("E_xit"), NULL, close_main_window, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_EXIT, 'Q', 0, NULL},
-    GNOMEUIINFO_END
-  };
-  static GnomeUIInfo message_menu[] = {
-    /* M */
-    {GNOME_APP_UI_ITEM, N_("_New"), NULL, new_message_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL, 'M', 0, NULL},
-    /* R */
-    {GNOME_APP_UI_ITEM, N_("_Reply"), NULL, replyto_message_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_RPL, 'R', 0, NULL},
-    /* A */
-    {GNOME_APP_UI_ITEM, N_("Reply to _all"), NULL, replytoall_message_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_RPL, 'A', 0, NULL},
-    /* F */
-    {GNOME_APP_UI_ITEM, N_("_Forward"), NULL, forward_message_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_MAIL_FWD, 'F', 0, NULL},
-    GNOMEUIINFO_SEPARATOR,
-    /* D */
-    {GNOME_APP_UI_ITEM, N_("_Delete"), NULL, delete_message_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_TRASH, 'D', 0, NULL},
-    /* U */
-    {GNOME_APP_UI_ITEM, N_("_Undelete"), NULL, undelete_message_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_UNDELETE, 'U', 0, NULL},
-    GNOMEUIINFO_END
-  };
-  static GnomeUIInfo mailbox_menu[] = {
-    /* C */
-    {GNOME_APP_UI_ITEM, N_("List"), NULL, mblist_window_cb, NULL,
-     NULL, GNOME_APP_PIXMAP_STOCK, GNOME_STOCK_MENU_PROP, 'C', 0, NULL},
-    GNOMEUIINFO_ITEM_STOCK("Close", NULL, mailbox_close_child, GNOME_STOCK_MENU_CLOSE),
-    GNOMEUIINFO_SEPARATOR,
-    GNOMEUIINFO_END
-  };
-  static GnomeUIInfo settings_menu[] = {
-    GNOMEUIINFO_ITEM_STOCK("_Filters...", NULL, filter_dlg_cb, GNOME_STOCK_MENU_PROP),
-    GNOMEUIINFO_ITEM_STOCK("_Preferences...", NULL, open_preferences_manager, GNOME_STOCK_MENU_PROP),
-    GNOMEUIINFO_END
-  };
-  static GnomeUIInfo help_menu[] = {
-    GNOMEUIINFO_ITEM_STOCK("_About...", NULL, show_about_box, GNOME_STOCK_MENU_ABOUT),
-    GNOMEUIINFO_HELP("balsa"),
-    GNOMEUIINFO_END
-  };
-  static GnomeUIInfo main_menu[] = {
-    GNOMEUIINFO_SUBTREE("_File", file_menu),
-    GNOMEUIINFO_SUBTREE("_Message", message_menu),
-    GNOMEUIINFO_SUBTREE("Mail_boxes", mailbox_menu),
-    GNOMEUIINFO_SUBTREE("_Settings", settings_menu),
-    GNOMEUIINFO_SUBTREE("_Help", help_menu),
-    GNOMEUIINFO_END
-  };
-
-  gnome_mdi_set_menubar_template(mdi, main_menu);
-}
-
-
-
-
-/*
- * the toolbar for the main window
- */
-static GtkToolbar *
-create_toolbar (GnomeMDI * mdi, GnomeApp * app)
-{
-  GtkWidget *window;
-  GtkWidget *toolbar;
-  GtkWidget *toolbarbutton;
-
-  toolbar = gtk_toolbar_new (GTK_ORIENTATION_HORIZONTAL, GTK_TOOLBAR_ICONS);
-
-  window = GTK_WIDGET (app);
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Check"),
-			     _ ("Check Email"),
-			     NULL,
-	    gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_MAIL_RCV),
-			     GTK_SIGNAL_FUNC (check_new_messages_cb),
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-
-  gtk_toolbar_append_space (GTK_TOOLBAR (toolbar));
-
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Delete"),
-			     _ ("Delete Message"),
-			     NULL,
-	       gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_TRASH),
-			     (GtkSignalFunc) delete_message_cb,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-
-  gtk_toolbar_append_space (GTK_TOOLBAR (toolbar));
-
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Compose"),
-			     _ ("Compose Message"),
-			     NULL,
-	    gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_MAIL_NEW),
-			     (GtkSignalFunc) new_message_cb,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Reply"),
-			     _ ("Reply"),
-			     NULL,
-	    gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_MAIL_RPL),
-			     (GtkSignalFunc) replyto_message_cb,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Reply to all"),
-			     _ ("Reply to all"),
-			     NULL,
-	    gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_MAIL_RPL),
-			     (GtkSignalFunc) replytoall_message_cb,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Forward"),
-			     _ ("Forward"),
-			     NULL,
-	    gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_MAIL_FWD),
-			     (GtkSignalFunc) forward_message_cb,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-
-  gtk_toolbar_append_space (GTK_TOOLBAR (toolbar));
-
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Previous"),
-			     _ ("Open Previous Message"),
-			     NULL,
-		gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_BACK),
-			     (GtkSignalFunc) previous_message_cb,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Next"),
-			     _ ("Open Next Message"),
-			     NULL,
-	     gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_FORWARD),
-			     (GtkSignalFunc) next_message_cb,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-
-
-  gtk_toolbar_append_space (GTK_TOOLBAR (toolbar));
-
-
-  toolbarbutton =
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar),
-			     _ ("Print"),
-			     _ ("Print current message"),
-			     NULL,
-	       gnome_stock_pixmap_widget (window, GNOME_STOCK_PIXMAP_PRINT),
-			     (GtkSignalFunc) NULL,
-			     NULL);
-  GTK_WIDGET_UNSET_FLAGS (toolbarbutton, GTK_CAN_FOCUS);
-
-  gtk_toolbar_set_style (GTK_TOOLBAR (toolbar), balsa_app.toolbar_style);
-
-  return GTK_TOOLBAR (toolbar);
-}
-
 
 /*
  * show the about box for Balsa
@@ -494,11 +393,11 @@ replyto_message_cb (GtkWidget * widget, gpointer data)
   if (!balsa_app.current_index_child)
     return;
 
-  clist = GTK_CLIST (BALSA_INDEX(balsa_app.current_index_child->index)->clist);
+  clist = GTK_CLIST (BALSA_INDEX (balsa_app.current_index_child->index)->clist);
   list = clist->selection;
   while (list)
     {
-      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT(list->data));
+      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT (list->data));
       sendmsg_window_new (widget, message, SEND_REPLY);
       list = list->next;
     }
@@ -516,11 +415,11 @@ replytoall_message_cb (GtkWidget * widget, gpointer data)
   if (!balsa_app.current_index_child)
     return;
 
-  clist = GTK_CLIST (BALSA_INDEX(balsa_app.current_index_child->index)->clist);
+  clist = GTK_CLIST (BALSA_INDEX (balsa_app.current_index_child->index)->clist);
   list = clist->selection;
   while (list)
     {
-      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT(list->data));
+      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT (list->data));
       sendmsg_window_new (widget, message, SEND_REPLY_ALL);
       list = list->next;
     }
@@ -539,11 +438,11 @@ forward_message_cb (GtkWidget * widget, gpointer data)
   if (!balsa_app.current_index_child)
     return;
 
-  clist = GTK_CLIST (BALSA_INDEX(balsa_app.current_index_child->index)->clist);
+  clist = GTK_CLIST (BALSA_INDEX (balsa_app.current_index_child->index)->clist);
   list = clist->selection;
   while (list)
     {
-      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT(list->data));
+      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT (list->data));
       sendmsg_window_new (widget, message, SEND_FORWARD);
       list = list->next;
     }
@@ -584,11 +483,11 @@ delete_message_cb (GtkWidget * widget, gpointer data)
   if (!balsa_app.current_index_child)
     return;
 
-  clist = GTK_CLIST (BALSA_INDEX(balsa_app.current_index_child->index)->clist);
+  clist = GTK_CLIST (BALSA_INDEX (balsa_app.current_index_child->index)->clist);
   list = clist->selection;
   while (list)
     {
-      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT(list->data));
+      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT (list->data));
       message_delete (message);
       list = list->next;
     }
@@ -607,11 +506,11 @@ undelete_message_cb (GtkWidget * widget, gpointer data)
   if (!balsa_app.current_index_child)
     return;
 
-  clist = GTK_CLIST (BALSA_INDEX(balsa_app.current_index_child->index)->clist);
+  clist = GTK_CLIST (BALSA_INDEX (balsa_app.current_index_child->index)->clist);
   list = clist->selection;
   while (list)
     {
-      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT(list->data));
+      message = gtk_clist_get_row_data (clist, GPOINTER_TO_INT (list->data));
       message_undelete (message);
       list = list->next;
     }

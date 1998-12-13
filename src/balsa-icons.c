3d5c53043254d342acc3f84371e2c59cfd9b0d7b
Stuart Parmenter <pavlov@src.gnome.org>
1998-12-13 21:34:17 +0000

    temp fix for icons

--- src/balsa-icons.c
+++ src/balsa-icons.c
@@ -60,13 +60,22 @@ static BalsaIcon envelope;
 static void
 imlib_magic_stuff (gchar ** data, GdkPixmap ** pmap, GdkBitmap ** bmap)
 {
+#ifdef IMLIB_WORKS_CORRECTLY
   GdkImlibImage *im;
   im = gdk_imlib_create_image_from_xpm_data (data);
   gdk_imlib_render (im, im->rgb_width, im->rgb_height);
-  *pmap = gdk_imlib_move_image (im);
-  *bmap = gdk_imlib_move_mask (im);
+  *pmap = gdk_imlib_copy_image (im);
+  *bmap = gdk_imlib_copy_mask (im);
   gdk_imlib_destroy_image (im);
   im = NULL;
+#endif
+
+  *pmap = gdk_pixmap_colormap_create_from_xpm_d(NULL,
+		  NULL,
+		  bmap,
+		  0,
+		  data);
+
 }
 
 void

356859bea8705f02a89900e2933bda11e408ee28
Hector Garcia <hectorg@src.gnome.org>
1999-12-05 00:12:32 +0000

    SMTP support rewrited and ofline message writing support added

--- libbalsa/send.c
+++ libbalsa/send.c
@@ -29,6 +29,8 @@
 #include "mailbackend.h"
 #include "send.h"
 
+#include "mime.h"
+
 /* This is temporary */
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -46,7 +48,9 @@
  */
 int mutt_send_message (HEADER * msg);
 static void encode_descriptions (BODY * b);
-int balsa_smtp_send (HEADER * msg);
+int balsa_smtp_send (HEADER *msg, char *file, char *server);
+int balsa_smtp_protocol (int s, char *tempfile, HEADER *msg);
+gboolean balsa_create_msg (Message * message, HEADER *msg, char *tempfile, int queu);
 
 BODY *add_mutt_body_plain (void);
 
@@ -102,105 +106,36 @@ gboolean
 balsa_send_message (Message * message)
 {
   HEADER *msg;
-  BODY *last, *newbdy;
-  gchar *tmp;
-  GList *list;
   int i=0;
+  char tempfile[_POSIX_PATH_MAX];
 
   msg = mutt_new_header ();
 
-  if (!msg->env)
-    msg->env = mutt_new_envelope ();
+  balsa_create_msg (message, msg, tempfile, 0);
 
-  msg->env->userhdrs = mutt_new_list ();
-  {
-    LIST *sptr = UserHeader;
-    LIST *dptr = msg->env->userhdrs;
-    LIST *delptr = 0;
-    while (sptr)
-      {
-	dptr->data = g_strdup (sptr->data);
-	sptr = sptr->next;
-	delptr = dptr;
-	dptr->next = mutt_new_list ();
-	dptr = dptr->next;
-      }
-    g_free (delptr->next);
-    delptr->next = 0;
-  }
-
-  tmp = address_to_gchar (message->from);
-  msg->env->from = rfc822_parse_adrlist (msg->env->from, tmp);
-  g_free (tmp);
-
-  tmp = address_to_gchar (message->reply_to);
-  msg->env->reply_to = rfc822_parse_adrlist (msg->env->reply_to, tmp);
-  g_free (tmp);
-
-  msg->env->subject = g_strdup (message->subject);
-
-  msg->env->to = rfc822_parse_adrlist (msg->env->to, make_string_from_list (message->to_list));
-  msg->env->cc = rfc822_parse_adrlist (msg->env->cc, make_string_from_list (message->cc_list));
-  msg->env->bcc = rfc822_parse_adrlist (msg->env->bcc, make_string_from_list (message->bcc_list));
-
-  list = message->body_list;
-
-  last = msg->content;
-  while (last && last->next)
-    last = last->next;
-
-  while (list)
-    {
-      FILE *tempfp = NULL;
-      Body *body;
-      newbdy = NULL;
-
-      body = list->data;
-
-      if (body->filename)
-	newbdy = mutt_make_file_attach (body->filename);
-
-      else if (body->buffer)
-	{
-	  newbdy = add_mutt_body_plain ();
-	  tempfp = safe_fopen (newbdy->filename, "w+");
-	  fputs (body->buffer, tempfp);
-	  fclose (tempfp);
-	  tempfp = NULL;
-	}
-
-      if (newbdy)
-	{
-	  if (last)
-	    last->next = newbdy;
-	  else
-	    msg->content = newbdy;
-
-	  last = newbdy;
-	}
-
-      list = list->next;
-    }
-
-  if (msg->content)
-    {
-      if (msg->content->next)
-	msg->content = mutt_make_multipart (msg->content);
-    }
-
-  mutt_prepare_envelope (msg->env);
-
-  encode_descriptions (msg->content);
-
-/* FIXME */
-  if (balsa_app.smtp)
-    i=balsa_smtp_send (msg);
-  else
-    mutt_send_message (msg);
+  if (balsa_app.smtp) 
+     i = balsa_smtp_send (msg,tempfile,balsa_app.smtp_server);
+  else	        
+     i = mutt_invoke_sendmail (msg->env->to, msg->env->cc, msg->env->bcc,tempfile,(msg->content->encoding == ENC8BIT));
   
   if (i==-1)
-     balsa_postpone_message (message);
-	     
+  {
+    mutt_write_fcc (MAILBOX_LOCAL (balsa_app.outbox)->path, msg, NULL, 1);
+    if (balsa_app.outbox->open_ref > 0)
+       mailbox_check_new_messages (balsa_app.outbox);
+    mutt_free_header (&msg);
+
+ /* Since we didn´t send the mail we don´t save it at send_mailbox */
+    return TRUE;
+  }
+
+
+  
+/**********************************************************************
+ * Here we continue with normal old file
+ * ********************************************************************/
+  
+  
   if ((balsa_app.sentbox->type == MAILBOX_MAILDIR ||
        balsa_app.sentbox->type == MAILBOX_MH ||
        balsa_app.sentbox->type == MAILBOX_MBOX) &&
@@ -334,7 +269,9 @@ balsa_postpone_message (Message * message)
   return TRUE;
 }
 
-/* In smtp_answer the check the answer and give back 1 if is a positive answer or a 0 if it is negative, it that case we need to copy the message to be send later. The error will be shown to the user from this funtion */	 
+/* In smtp_answer the check the answer and give back 1 if is a positive answer or a 0 if it is negative, 
+ * in that case we need to copy the message to be send later. The error will be shown to the user from 
+ * this funtion */	 
    
 static int smtp_answer (int fd)
   {
@@ -342,9 +279,9 @@ static int smtp_answer (int fd)
   char code[4]; /* we use the 3 number code to check the answer */
   int bytes = 0;
   
-  tmp=&buffer; 
+  tmp = buffer; 
 		  
-  bytes=read (fd, tmp, sizeof(buffer));	  
+  bytes = (int)read (fd, tmp, sizeof(buffer));	  
    
  
   /* now we check the posible answers */
@@ -367,131 +304,59 @@ static int smtp_answer (int fd)
     }
 }
 
-/* This funtion uses the same arguments as mutt_send_message. 
- * I copy part of the mutt_send_message funtion instead of making the call to
- * this funtion in the last minute, because in this way we don't have to 
- * worry about updating libmutt */
+/* This funtion recives as arguments the message (to use headers),
+ * a file were the message is in MIME format (converted using libmutt)
+ * and the server (so it will be easier to add support for multiple 
+ * personalityes) and it returns 1 if everything is ok or 0 if something
+ * when wrong  */
 
-
-int balsa_smtp_send (HEADER * msg)
+int balsa_smtp_protocol (int s, char *tempfile, HEADER *msg)
 {
-
-  struct sockaddr_in sin;
-  struct hostent *he;
   char message[1000], buffer[525]; /* Maximum allow by RFC */
-  int fp,left,len,n,s,SmtpPort=25;
-  char *tmp, *tmpbuffer, tempfile[_POSIX_PATH_MAX];
-  FILE *tempfp;
+  int fp,left,len;
+  char *tmp, *tmpbuffer;
   ADDRESS *address;
-  
-/* This comes from mutt_send_message */ 
-
-  /* Write out the message in MIME form. */
-  mutt_mktemp (tempfile);
-  if ((tempfp = safe_fopen (tempfile, "w")) == NULL)
-      return (-1);
-
-  mutt_write_rfc822_header (tempfp, msg->env, msg->content, 0);
-  fputc ('\n', tempfp); /* tie off the header. */
-
-  if ((mutt_write_mime_body (msg->content, tempfp) == -1))
-      {
-      fclose(tempfp);
-      unlink (tempfile);
-      return (-1);
-      }
-  fputc ('\n', tempfp); /* tie off the body. */
-
-  if (fclose (tempfp) != 0)
-     {
-     mutt_perror (tempfile);
-     unlink (tempfile);
-     return (-1);
-     }
-/* Now we have the body in a temporary file */
-
-/* Here we make the conecction */  
-  s = socket (AF_INET, SOCK_STREAM, IPPROTO_IP);
-
-  memset ((char *) &sin, 0, sizeof(sin));
-  sin.sin_family = AF_INET;
-  sin.sin_port = htons (SmtpPort);
-
-  if ((n = inet_addr (NONULL(balsa_app.smtp_server))) == -1)
-  {
-    /* Must be a DNS name */
-    if ((he = gethostbyname (NONULL(balsa_app.smtp_server))) == NULL)
-    {
-      fprintf(stderr,"Error: Could not find address for host %s.", balsa_app.smtp_server);
-      //mutt_error ("Could not find address for host %s.", balsa_app.smtp_server);
-      return -1;
-    }
-    memcpy ((void *)&sin.sin_addr, *(he->h_addr_list), he->h_length);
-  }
-  else
-    memcpy ((void *)&sin.sin_addr, (void *)&n, sizeof(n));
-  
-  //mutt_message ("Connecting to %s", inet_ntoa (sin.sin_addr));
-
-  if (connect (s, (struct sockaddr *) &sin, sizeof (struct sockaddr_in)) == -1)
-  {
-    fprintf(stderr,"Error connecting");
-    //mutt_perror ("connect");
-    return -1;
-  }
-  
-/* Here we have to receive whatever is the initial salutation of the smtp server, since now we are not going to make use of the server being esmtp, we don´t care about this salutation */
  
-  if (smtp_answer (s) == 0)
-    return -1;
-
-/* Here I just follow the RFC */
-	  
-  snprintf (buffer, 512, "HELO %s\r\n", balsa_app.smtp_server);
-  write (s, buffer, strlen (buffer));
-	  
-  if (smtp_answer (s) == 0)
-    return -1;
-
   snprintf (buffer, 512,"MAIL FROM:%s\r\n", msg->env->from->mailbox);
   write (s, buffer, strlen (buffer));
   if (smtp_answer (s) == 0)
-    return -1;
+    return 0;
 
-  address=msg->env->to;
-  while (address!=NULL)
+  address = msg->env->to;
+  while (address != NULL)
   {	  
       snprintf (buffer, 512,"RCPT TO: %s\r\n", address->mailbox);
       write (s, buffer, strlen (buffer));
       address=address->next;
-    /* We check for a positive answer */    
+    /* We check for a positive answer */
+        
       if (smtp_answer (s) == 0)
-         return -1;
+         return 0;
     
      /*  let's go to the next address */
       
   }
 
-  address=msg->env->cc;
-  while (address!=NULL)
+  address = msg->env->cc;
+  while (address != NULL)
   {
       snprintf (buffer, sizeof(buffer),"RCPT TO: %s\r\n", address->mailbox);
       write (s, buffer, strlen (buffer));
       address=address->next;
 
       if (smtp_answer (s) == 0)
-          return -1;
+          return 0;
    }
 
-  address=msg->env->bcc;
-  while (address!=NULL)
+  address = msg->env->bcc;
+  while (address != NULL)
   {       
       snprintf (buffer, sizeof(buffer),"RCPT TO:%s\r\n", address->mailbox);
       write (s, buffer, strlen (buffer));
       address=address->next;
 
       if (smtp_answer (s) == 0)
-          return -1;
+          return 0;
    }
 
 
@@ -500,17 +365,18 @@ int balsa_smtp_send (HEADER * msg)
   snprintf (buffer, 512,"DATA\r\n");
   write (s, buffer, strlen (buffer));
   if (smtp_answer (s) == 0)
-    return -1;
+    return 0;
 
   if ((fp=open(tempfile,O_RDONLY))==-1)
     return -1;
 
-  while ((left=read (fp, message, sizeof(message)))!=0)
+  while ((left = (int)read (fp, message, sizeof(message)))!=0)
   {
-    tmpbuffer=&message;
-    while ((tmp=strstr(tmpbuffer,"\n"))!=NULL)
+    tmpbuffer = message;
+    while ((tmp = strstr(tmpbuffer,"\n"))!=NULL)
     {
-	    len=strcspn(tmpbuffer,"\n");
+	    len = (int)strcspn(tmpbuffer,"\n");
+	    
 	    if (*(tmp-1)!='\r')
             {
 		write (s,tmpbuffer,len);
@@ -528,23 +394,284 @@ int balsa_smtp_send (HEADER * msg)
     }
     
     write (s, tmpbuffer,left);  
-  
+    
   }
   
   snprintf (buffer, sizeof(buffer),"\r\n.\r\n");
   write (s, buffer, strlen (buffer));
 
   if (smtp_answer (s) == 0)
+    return 0;
+
+  close (fp);
+  
+  return 1;
+}
+
+int balsa_smtp_send (HEADER *msg, char *tempfile, char *server)
+{
+
+  struct sockaddr_in sin;
+  struct hostent *he;
+  char buffer[525]; /* Maximum allow by RFC */
+  int n, s, error = 0, SmtpPort=25;
+  GList *lista;
+  Message *message;
+  HEADER *queu_msg;
+  char file[_POSIX_PATH_MAX];
+  
+/* Here we make the conecction */  
+  s = socket (AF_INET, SOCK_STREAM, IPPROTO_IP);
+
+  memset ((char *) &sin, 0, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons (SmtpPort);
+
+  if ((n = inet_addr (NONULL(server))) == -1)
+  {
+    /* Must be a DNS name */
+    if ((he = gethostbyname (NONULL(balsa_app.smtp_server))) == NULL)
+    {
+      fprintf(stderr,"Error: Could not find address for host %s.\n", server);
+      //mutt_error ("Could not find address for host %s.", balsa_app.smtp_server);
+      return -1;
+    }
+    memcpy ((void *)&sin.sin_addr, *(he->h_addr_list), he->h_length);
+  }
+  else
+    memcpy ((void *)&sin.sin_addr, (void *)&n, sizeof(n));
+  
+  mutt_message ("Connecting to %s", inet_ntoa (sin.sin_addr));
+
+  if (connect (s, (struct sockaddr *) &sin, sizeof (struct sockaddr_in)) == -1)
+  {
+    fprintf(stderr,"Error connecting to host\n\n");
+    //mutt_perror ("connect");
+    return -1;
+  }
+  
+/* Here we have to receive whatever is the initial salutation of the smtp server, since now we are not going to make use of the server being esmtp, we don´t care about this salutation */
+ 
+  if (smtp_answer (s) == 0)
     return -1;
 
+/* Here I just follow the RFC */
+	  
+  snprintf (buffer, 512, "HELO %s\r\n", server);
+  write (s, buffer, strlen (buffer));
+	  
+  if (smtp_answer (s) == 0)
+    return -1;
+
+  if ((balsa_smtp_protocol ( s, tempfile, msg)) == 0 )
+  {
+     error = 1;
+     snprintf (buffer, 512, "RSET\r\n", server);
+     write (s, buffer, strlen (buffer));
+     if (smtp_answer (s) == 0)
+        return -1;
+  }
+  
+  unlink (tempfile);
+
+  
+   /* We do messages in queu now */
+   
+  balsa_mailbox_open (balsa_app.outbox);
+  
+  if (balsa_app.outbox->message_list != NULL)
+      lista = balsa_app.outbox->message_list ;
+  else
+      lista = NULL ;
+  
+  while (lista != NULL)
+  {
+ 
+    message = LIBBALSA_MESSAGE(lista->data); 
+    queu_msg = mutt_new_header ();
+  
+    balsa_create_msg (message, queu_msg, file, 1);
+  
+  /* If something happen we do not delete the message */
+    
+    if ((balsa_smtp_protocol ( s, file, queu_msg)) == 0 )
+    {
+       snprintf (buffer, 512, "RSET\r\n", server);
+       write (s, buffer, strlen (buffer));
+       if (smtp_answer (s) == 0)
+          return -1;
+    }
+    else
+    {
+       fprintf (stderr, "Mensaje borrado\n");
+       message_delete(message);
+    }
+  
+    lista = lista->next ;  
+  }
+    
+  balsa_mailbox_close (balsa_app.outbox);
+      
+  
 /* We close the conection */
+
   
   snprintf (buffer, sizeof(buffer),"QUIT\r\n");
   write (s, buffer, strlen (buffer));
 
   close (s);
-  close (fp);
-  unlink (tempfile);
+  
+  if (error == 1)
+     return -1;
+     
   return 1;
 }  
+
+
+gboolean balsa_create_msg (Message *message, HEADER *msg, char *tmpfile, int queu)
+{
+  BODY *last, *newbdy;
+  gchar *tmp;
+  GList *list;
+  FILE *tempfp;
+
+
+  if (!msg->env)
+    msg->env = mutt_new_envelope ();
+
+  msg->env->userhdrs = mutt_new_list ();
+  {
+    LIST *sptr = UserHeader;
+    LIST *dptr = msg->env->userhdrs;
+    LIST *delptr = 0;
+    while (sptr)
+      {
+	dptr->data = g_strdup (sptr->data);
+	sptr = sptr->next;
+	delptr = dptr;
+	dptr->next = mutt_new_list ();
+	dptr = dptr->next;
+      }
+    g_free (delptr->next);
+    delptr->next = 0;
+  }
+
+  tmp = address_to_gchar (message->from);
+  msg->env->from = rfc822_parse_adrlist (msg->env->from, tmp);
+  g_free (tmp);
+
+  tmp = address_to_gchar (message->reply_to);
+  msg->env->reply_to = rfc822_parse_adrlist (msg->env->reply_to, tmp);
+  g_free (tmp);
+
+  msg->env->subject = g_strdup (message->subject);
+
+  msg->env->to = rfc822_parse_adrlist (msg->env->to, make_string_from_list (message->to_list));
+  msg->env->cc = rfc822_parse_adrlist (msg->env->cc, make_string_from_list (message->cc_list));
+  msg->env->bcc = rfc822_parse_adrlist (msg->env->bcc, make_string_from_list (message->bcc_list));
+
+  if ((list = message->body_list) == NULL )
+  {
+     message_body_ref (message);
+     list = message->body_list;
+  }
+  
+  last = msg->content;
+  while (last && last->next)
+     last = last->next;
+
+  while (list)
+    {
+      FILE *tempfp = NULL;
+      Body *body;
+      newbdy = NULL;
+
+      body = list->data;
+
+      if (body->filename)
+	newbdy = mutt_make_file_attach (body->filename);
+
+      else if (body->buffer)
+	{
+	  newbdy = add_mutt_body_plain ();
+	  tempfp = safe_fopen (newbdy->filename, "w+");
+	  fputs (body->buffer, tempfp);
+	  fclose (tempfp);
+	  tempfp = NULL;
+	}
+      else
+        msg->content = body->mutt_body ;
+
+      if (newbdy)
+	{
+	  if (last)
+	    last->next = newbdy;
+	  else
+	    msg->content = newbdy;
+
+	  last = newbdy;
+	}
+
+      list = list->next;
+    }
+
+  if (msg->content)
+    {
+      if (msg->content->next)
+	msg->content = mutt_make_multipart (msg->content);
+    }
+
+  mutt_prepare_envelope (msg->env);
+
+  encode_descriptions (msg->content);
+
+/* We create the message in MIME format here, we use the same format 
+ * for local delivery that for SMTP */
+  if (queu == 0)
+  {
+	mutt_mktemp (tmpfile);
+//	file = &tmpfile;
+	if ((tempfp = safe_fopen (tmpfile, "w")) == NULL)
+ 	     return (-1);
+
+	mutt_write_rfc822_header (tempfp, msg->env, msg->content, 0);
+	fputc ('\n', tempfp); /* tie off the header. */
+
+	if ((mutt_write_mime_body (msg->content, tempfp) == -1))
+	{
+ 	     fclose(tempfp);
+ 	     unlink (tmpfile);
+  	     return (-1);
+   	}
+  	fputc ('\n', tempfp); /* tie off the body. */
+
+  	if (fclose (tempfp) != 0)
+   	{
+    	     mutt_perror (tmpfile);
+             unlink (tmpfile);
+             return (-1);
+        }
+
+  }
+  else
+  {
+  	switch (message->mailbox->type)
+        {
+		case MAILBOX_MH:
+  		case MAILBOX_MAILDIR:
+    		  {
+      			  snprintf (tmpfile, PATH_MAX, "%s/%s", MAILBOX_LOCAL (message->mailbox)->path, message_pathname (message));
+  //      		  file = tmpfile;
+        		  break;
+      		  }
+                default:
+  //                  file = fopen (MAILBOX_LOCAL (message->mailbox)->path, "r");
+   	            break;
+	}
+
+  }	 
+
+        return TRUE;
+        
+}
 

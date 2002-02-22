8b39e05f73c75397fff66ec293aa49cb433d8dd5
Pawel Salek <pawels@src.gnome.org>
2002-02-22 09:24:02 +0000

    Fix some mistakes found in configure.in. Adapt autogen.sh to gnome2.

--- autogen.sh
+++ autogen.sh
@@ -4,15 +4,41 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="Gnome Balsa"
+PKG_NAME="Balsa"
 
 (test -f $srcdir/configure.in \
-  && test -d $srcdir/libmutt \
-  && test -d $srcdir/src) || {
+  && test -d $srcdir/src \
+  && test -f $srcdir/src/balsa-app.h) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level balsa directory"
-
     exit 1
 }
 
-. $srcdir/macros/autogen.sh
+if libtool --version >/dev/null 2>&1; then
+    vers=`libtool --version | sed -e "s/^[^0-9]*//" -e "s/ .*$//" | awk 'BEGIN { FS = "."; } { printf "%d", ($1 * 1000 + $2) * 1000 + $3;}'`
+    if test "$vers" -ge 1003003; then
+        true
+    else
+        echo "Please upgrade your libtool to version 1.3.3 or better." 1>&2
+        exit 1
+    fi
+fi
+
+ifs_save="$IFS"; IFS=":"
+for dir in $PATH ; do
+  test -z "$dir" && dir=.
+  if test -f $dir/gnome-autogen.sh ; then
+    gnome_autogen="$dir/gnome-autogen.sh"
+    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
+    break
+  fi
+done
+IFS="$ifs_save"
+
+if test -z "$gnome_autogen" ; then
+  echo "You need to install the gnome-common module and make"
+  echo "sure the gnome-autogen.sh script is in your \$PATH."
+  exit 1
+fi
+
+GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen

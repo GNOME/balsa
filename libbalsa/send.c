23ac44d68b37c4f51bedb541f5fe3c72f2b32b83
Pawel Salek <pawels@src.gnome.org>
2002-09-23 09:11:32 +0000

    do not send deleted messages in outbox (#93666).
    * libbalsa/send.c: do not send deleted messages in outbox (#93666).

--- libbalsa/send.c
+++ libbalsa/send.c
@@ -559,7 +559,9 @@ libbalsa_process_queue(LibBalsaMailbox* outbox, gint encoding,
     for (lista = outbox->message_list; lista; lista = lista->next) {
 	msg = LIBBALSA_MESSAGE(lista->data);
 
-        if(LIBBALSA_MESSAGE_IS_FLAGGED(msg)) continue;
+	if (msg->flags & (LIBBALSA_MESSAGE_FLAG_FLAGGED |
+                          LIBBALSA_MESSAGE_FLAG_DELETED))
+            continue;
 	new_message = msg_queue_item_new(msg);
 	if (!libbalsa_create_msg(msg, new_message->message,
 				 new_message->tempfile, encoding, 

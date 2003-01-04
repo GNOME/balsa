7816937ff01aed241f9c5e0151167314c357d286
Pawel Salek <pawels@src.gnome.org>
2003-01-04 09:46:29 +0000

    change default action for D&D (#102140).
    * src/balsa-app.[hc]: change default action for D&D (#102140).

--- src/balsa-app.c
+++ src/balsa-app.c
@@ -435,7 +435,7 @@ balsa_app_init(void)
     /* Message filing */
     balsa_app.folder_mru=NULL;
     balsa_app.fcc_mru=NULL;
-    balsa_app.drag_default_is_move=0;
+    balsa_app.drag_default_is_move=TRUE;
 }
 
 /* Word of comment: previous definition of this function used access()

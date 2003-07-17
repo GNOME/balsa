58aa7b0145d7df34845011e8f9e58bcfbec66048
Peter Bloomfield <PeterB@src.gnome.org>
2003-07-17 15:29:21 +0000

    Leave thread expanded when deleting message(s) (http://mail.gnome.org/archives/balsa-list/2003-July/msg00054.html).

--- src/balsa-index.c
+++ src/balsa-index.c
@@ -2376,6 +2376,13 @@ bndx_copy_tree(BalsaIndex * index, GNode * node, GtkTreeIter * parent_iter)
 
     gtk_tree_store_append(GTK_TREE_STORE(model), &new_iter, parent_iter);
 
+    if (balsa_app.expand_tree && parent_iter) {
+	GtkTreePath *parent_path =
+	    gtk_tree_model_get_path(model, parent_iter);
+	gtk_tree_view_expand_row(GTK_TREE_VIEW(index), parent_path, FALSE);
+	gtk_tree_path_free(parent_path);
+    }
+
     gtk_tree_store_set(GTK_TREE_STORE(model), &new_iter,
                        BNDX_MESSAGE_COLUMN, message,
                        BNDX_INDEX_COLUMN, num,

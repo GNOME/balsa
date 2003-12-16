145f16fcc25f78a711950eb322e16ee62aaad9b7
Peter Bloomfield <PeterB@src.gnome.org>
2003-12-16 18:31:03 +0000

    Remove unnecessary mailbox locking.

--- libbalsa/mailbox_imap.c
+++ libbalsa/mailbox_imap.c
@@ -1417,9 +1417,7 @@ libbalsa_mailbox_imap_get_message(LibBalsaMailbox * mailbox, guint msgno)
         msg->msgno   = msgno;
         msg->mailbox = mailbox;
         if (libbalsa_mailbox_imap_load_envelope(mimap, msg)) {
-	    LOCK_MAILBOX_RETURN_VAL(LIBBALSA_MAILBOX(mimap),NULL);
 	    libbalsa_message_set_icons(msg);
-	    UNLOCK_MAILBOX(LIBBALSA_MAILBOX(mimap));
             msg_info->message  = msg;
 	} else 
             g_object_unref(G_OBJECT(msg));

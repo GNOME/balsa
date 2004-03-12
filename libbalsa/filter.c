aae311924b75012a3a3853bbffd255056655c98c
Pawel Salek <pawels@src.gnome.org>
2004-03-12 07:30:53 +0000

    compare filter names correctly.
    * libbalsa/filter.c: compare filter names correctly.

--- libbalsa/filter.c
+++ libbalsa/filter.c
@@ -440,13 +440,15 @@ libbalsa_filter_get_by_name(const gchar * fname)
     g_return_val_if_fail(filter_list, NULL);
     if (!fname || fname[0]=='\0') return NULL;
 
-    fnamelen=strlen(fname);
-    for (list= *filter_list;
-         list && 
-             strncmp(fname,((LibBalsaFilter*)list->data)->name,fnamelen)!=0;
-         list=g_slist_next(list))
-        ;
-    return list ? (LibBalsaFilter*)list->data : NULL;
+    fnamelen = strlen(fname);
+    for (list = *filter_list;list;list = g_slist_next(list)) {
+	gint len = strlen(((LibBalsaFilter*)list->data)->name);
+
+	if (strncmp(fname,((LibBalsaFilter*)list->data)->name,
+		    MAX(len,fnamelen))==0)
+	    return (LibBalsaFilter*)list->data;
+    }
+    return NULL;
 }
 
 /* Check whether the condition tests the message body, and if so,

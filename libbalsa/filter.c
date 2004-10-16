50668809c28ac59fb75555be6ef9b25193fbf530
Peter Bloomfield <PeterB@src.gnome.org>
2004-10-16 12:50:54 +0000

    do not call libbalsa_mailbox_search_iter_free--view filtering is now managed by mailbox code.

--- libbalsa/filter.c
+++ libbalsa/filter.c
@@ -282,7 +282,6 @@ libbalsa_filter_mailbox_messages(LibBalsaFilter * filt,
 {
     gboolean result=FALSE;
     LibBalsaMailbox *mbox;
-    LibBalsaMailboxSearchIter *iter_view;
 
     if (msgnos->len == 0)
 	return FALSE;
@@ -338,17 +337,6 @@ libbalsa_filter_mailbox_messages(LibBalsaFilter * filt,
     case FILTER_NOTHING:
 	/* Nothing to do */
 	break;
-    }
-
-    iter_view = libbalsa_mailbox_search_iter_view(mailbox);
-    if (iter_view) {
-	guint i;
-
-	for (i = 0; i < msgnos->len; i++)
-	    libbalsa_mailbox_msgno_filt_check(mailbox,
-					      g_array_index(msgnos, guint, i),
-					      iter_view);
-	libbalsa_mailbox_search_iter_free(iter_view);
     }
 
     libbalsa_unlock_mailbox(mailbox);

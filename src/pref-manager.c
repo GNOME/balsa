a7be122d6d53b0fba027a0dcfb8752591a5e405c
Pawel Salek <pawels@src.gnome.org>
2005-02-24 10:10:34 +0000

    compile with C89 (contributed by Jens Granseuer).
    * src/pref-manager.c: compile with C89 (contributed by Jens Granseuer).

--- src/pref-manager.c
+++ src/pref-manager.c
@@ -2457,13 +2457,14 @@ moving_messages_group(GtkWidget * page)
 {
     GtkWidget *group;
     GtkWidget *label;
+    GtkWidget *table;
 
     group = pm_group_new(_("Message Window"));
 
     label = gtk_label_new(_("Action after moving/trashing a message"));
     gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
 
-    GtkWidget *table = create_table(4, 2, page);
+    table = create_table(4, 2, page);
     gtk_table_attach(GTK_TABLE(table), label, 0, 1, 3, 4,
 		     (GtkAttachOptions) (GTK_FILL),
 		     (GtkAttachOptions) (0), 0, 0);

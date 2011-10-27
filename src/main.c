ec38e0f1e3a6a5cc72e890ce17addc5aa3d38d96
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2011-10-26 20:04:26 -0400

    Use g_type_init, and don't use g_threads_init
    	* src/main.c (threads_init): use g_type_init, which is required,
    	not g_threads_init, which is redundant.

--- src/main.c
+++ src/main.c
@@ -473,7 +473,7 @@ pthread_mutex_t checking_mail_lock = PTHREAD_MUTEX_INITIALIZER;
 static void
 threads_init(void)
 {
-    g_thread_init(NULL);
+    g_type_init();
 
     libbalsa_threads_init();
     gdk_threads_init();

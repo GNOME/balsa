2ab61da6cc0d3a4d2441c8e095359f9e3c541918
Pawel Salek <pawsa0@gmail.com>
2012-10-23 23:00:10 +0200

    Remove debug printf()s

--- libbalsa/imap/pop3.c
+++ libbalsa/imap/pop3.c
@@ -641,7 +641,6 @@ pop_fetch_message_s(PopHandle *pop, unsigned msgno,
       char *arg = line[0] == '.' ? line+1 : line;
       unsigned len = strlen(arg);
       continuation_line = (len >= POP_LINE_LEN-1);
-      printf("Line '%s'\n", line);
       if(pop->filter_cr && len>=2 && arg[len-2] == '\r') 
         arg[(--len)-1] = '\n';
       if(resp) 
@@ -651,7 +650,6 @@ pop_fetch_message_s(PopHandle *pop, unsigned msgno,
           resp = FALSE;
         }
     }
-    printf("LAST Line is '%s'\n", line);
   }
   return resp;
 }

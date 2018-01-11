cab81e4f25439f0b2c105f342932bd676b3c886e
Albrecht Dreß <albrecht.dress@arcor.de>
2018-01-11 13:37:55 -0500

    mailbox-pop3: Fix progress dialogue on error
    Signed-off-by: Peter Bloomfield <PeterBloomfield@bellsouth.net>

--- libbalsa/mailbox_pop3.c
+++ libbalsa/mailbox_pop3.c
@@ -623,7 +623,7 @@ libbalsa_mailbox_pop3_check(LibBalsaMailbox * mailbox)
 
 	server = LIBBALSA_MAILBOX_REMOTE_SERVER(mbox);
 
-	/* open the mailbox connection and get the messages list */
+	/* open the mailbox connection and get the messages list (note: initiates the progress dialogue) */
 	pop = libbalsa_mailbox_pop3_startup(server, mbox, mailbox->name, &msg_list);
 
 	/* proceed on success only */
@@ -690,8 +690,9 @@ libbalsa_mailbox_pop3_check(LibBalsaMailbox * mailbox)
 
 		/* done - clean up */
 		g_object_unref(G_OBJECT(pop));
-		libbalsa_mailbox_progress_notify(mailbox, LIBBALSA_NTFY_FINISHED, 1.0, _("Finished"));
 	}
+
+	libbalsa_mailbox_progress_notify(mailbox, LIBBALSA_NTFY_FINISHED, 1.0, _("Finished"));
 }
 
 

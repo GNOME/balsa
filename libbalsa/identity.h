9bae10c8be332737a997c38dcb6924c28ec2e5cf
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2018-03-12 12:00:00 -0400

    Change one dummy argument, for consistency

--- libbalsa/identity.h
+++ libbalsa/identity.h
@@ -40,59 +40,59 @@ G_DECLARE_FINAL_TYPE(LibBalsaIdentity,
                      GObject)
 
 /* Function prototypes */
-LibBalsaIdentity *libbalsa_identity_new(void);
+LibBalsaIdentity * libbalsa_identity_new(void);
 LibBalsaIdentity *libbalsa_identity_new_with_name(const gchar *ident_name);
 LibBalsaIdentity *libbalsa_identity_new_from_config(const gchar *name);
 void              libbalsa_identity_save(LibBalsaIdentity *ident,
                                          const gchar      *prefix);
 
 /* Setters */
-void     libbalsa_identity_set_identity_name(LibBalsaIdentity *ident,
-                                             const gchar      *name);
-void     libbalsa_identity_set_address(LibBalsaIdentity *ident,
-                                       InternetAddress  *ia);
-void     libbalsa_identity_set_replyto(LibBalsaIdentity *ident,
-                                       const gchar      *reply_to);
-void     libbalsa_identity_set_domain(LibBalsaIdentity *ident,
-                                      const gchar      *text);
-void     libbalsa_identity_set_bcc(LibBalsaIdentity *ident,
-                                   const gchar      *text);
-void     libbalsa_identity_set_reply_string(LibBalsaIdentity *ident,
+void libbalsa_identity_set_identity_name(LibBalsaIdentity *ident,
+                                         const gchar      *name);
+void libbalsa_identity_set_address(LibBalsaIdentity *ident,
+                                   InternetAddress  *ia);
+void libbalsa_identity_set_replyto(LibBalsaIdentity *ident,
+                                   const gchar      *replyto);
+void libbalsa_identity_set_domain(LibBalsaIdentity *ident,
+                                  const gchar      *text);
+void libbalsa_identity_set_bcc(LibBalsaIdentity *ident,
+                               const gchar      *text);
+void libbalsa_identity_set_reply_string(LibBalsaIdentity *ident,
+                                        const gchar      *text);
+void libbalsa_identity_set_forward_string(LibBalsaIdentity *ident,
+                                          const gchar      *text);
+void libbalsa_identity_set_send_mp_alternative(LibBalsaIdentity *ident,
+                                               gboolean          set);
+void libbalsa_identity_set_signature_path(LibBalsaIdentity *ident,
+                                          const gchar      *text);
+void libbalsa_identity_set_sig_executable(LibBalsaIdentity *ident,
+                                          gboolean          set);
+void libbalsa_identity_set_sig_sending(LibBalsaIdentity *ident,
+                                       gboolean          set);
+void libbalsa_identity_set_sig_whenforward(LibBalsaIdentity *ident,
+                                           gboolean          set);
+void libbalsa_identity_set_sig_whenreply(LibBalsaIdentity *ident,
+                                         gboolean          set);
+void libbalsa_identity_set_sig_separator(LibBalsaIdentity *ident,
+                                         gboolean          set);
+void libbalsa_identity_set_sig_prepend(LibBalsaIdentity *ident,
+                                       gboolean          set);
+void libbalsa_identity_set_face_path(LibBalsaIdentity *ident,
+                                     const gchar      *text);
+void libbalsa_identity_set_x_face_path(LibBalsaIdentity *ident,
+                                       const gchar      *text);
+void libbalsa_identity_set_request_mdn(LibBalsaIdentity *ident,
+                                       gboolean          set);
+void libbalsa_identity_set_request_dsn(LibBalsaIdentity *ident,
+                                       gboolean          set);
+void libbalsa_identity_set_always_trust(LibBalsaIdentity *ident,
+                                        gboolean          set);
+void libbalsa_identity_set_warn_send_plain(LibBalsaIdentity *ident,
+                                           gboolean          set);
+void libbalsa_identity_set_force_gpg_key_id(LibBalsaIdentity *ident,
                                             const gchar      *text);
-void     libbalsa_identity_set_forward_string(LibBalsaIdentity *ident,
+void libbalsa_identity_set_force_smime_key_id(LibBalsaIdentity *ident,
                                               const gchar      *text);
-void     libbalsa_identity_set_send_mp_alternative(LibBalsaIdentity *ident,
-                                                   gboolean          set);
-void     libbalsa_identity_set_signature_path(LibBalsaIdentity *ident,
-                                              const gchar      *text);
-void     libbalsa_identity_set_sig_executable(LibBalsaIdentity *ident,
-                                              gboolean          set);
-void     libbalsa_identity_set_sig_sending(LibBalsaIdentity *ident,
-                                           gboolean          set);
-void     libbalsa_identity_set_sig_whenforward(LibBalsaIdentity *ident,
-                                               gboolean          set);
-void     libbalsa_identity_set_sig_whenreply(LibBalsaIdentity *ident,
-                                             gboolean          set);
-void     libbalsa_identity_set_sig_separator(LibBalsaIdentity *ident,
-                                             gboolean          set);
-void     libbalsa_identity_set_sig_prepend(LibBalsaIdentity *ident,
-                                           gboolean          set);
-void     libbalsa_identity_set_face_path(LibBalsaIdentity *ident,
-                                         const gchar      *text);
-void     libbalsa_identity_set_x_face_path(LibBalsaIdentity *ident,
-                                           const gchar      *text);
-void     libbalsa_identity_set_request_mdn(LibBalsaIdentity *ident,
-                                           gboolean          set);
-void     libbalsa_identity_set_request_dsn(LibBalsaIdentity *ident,
-                                           gboolean          set);
-void     libbalsa_identity_set_always_trust(LibBalsaIdentity *ident,
-                                            gboolean          set);
-void     libbalsa_identity_set_warn_send_plain(LibBalsaIdentity *ident,
-                                               gboolean          set);
-void     libbalsa_identity_set_force_gpg_key_id(LibBalsaIdentity *ident,
-                                                const gchar      *text);
-void     libbalsa_identity_set_force_smime_key_id(LibBalsaIdentity *ident,
-                                                  const gchar      *text);
 
 gchar *libbalsa_identity_get_signature(LibBalsaIdentity *ident,
                                        GError          **error);
@@ -111,33 +111,33 @@ void libbalsa_identity_set_crypt_protocol(LibBalsaIdentity *ident,
  * Getters
  */
 
-gboolean     libbalsa_identity_get_sig_prepend(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_sig_whenreply(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_sig_whenforward(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_sig_sending(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_send_mp_alternative(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_request_mdn(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_request_dsn(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_warn_send_plain(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_always_trust(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_gpg_sign(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_gpg_encrypt(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_sig_executable(LibBalsaIdentity *ident);
-gboolean     libbalsa_identity_get_sig_separator(LibBalsaIdentity *ident);
-gint         libbalsa_identity_get_crypt_protocol(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_identity_name(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_force_gpg_key_id(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_force_smime_key_id(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_replyto(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_bcc(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_reply_string(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_forward_string(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_domain(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_face_path(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_x_face_path(LibBalsaIdentity *ident);
-const gchar *libbalsa_identity_get_signature_path(LibBalsaIdentity *ident);
-InternetAddress *libbalsa_identity_get_address(LibBalsaIdentity *ident);
-LibBalsaSmtpServer * libbalsa_identity_get_smtp_server(LibBalsaIdentity * ident);
+gboolean            libbalsa_identity_get_sig_prepend(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_sig_whenreply(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_sig_whenforward(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_sig_sending(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_send_mp_alternative(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_request_mdn(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_request_dsn(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_warn_send_plain(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_always_trust(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_gpg_sign(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_gpg_encrypt(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_sig_executable(LibBalsaIdentity *ident);
+gboolean            libbalsa_identity_get_sig_separator(LibBalsaIdentity *ident);
+gint                libbalsa_identity_get_crypt_protocol(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_identity_name(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_force_gpg_key_id(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_force_smime_key_id(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_replyto(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_bcc(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_reply_string(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_forward_string(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_domain(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_face_path(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_x_face_path(LibBalsaIdentity *ident);
+const gchar        *libbalsa_identity_get_signature_path(LibBalsaIdentity *ident);
+InternetAddress    *libbalsa_identity_get_address(LibBalsaIdentity *ident);
+LibBalsaSmtpServer *libbalsa_identity_get_smtp_server(LibBalsaIdentity *ident);
 
 G_END_DECLS
 

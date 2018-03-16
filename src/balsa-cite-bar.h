9dd47db54c90764eb4020bdb48f56a781ae92e32
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2018-03-15 20:52:48 -0400

    citebar: declare it final
    Use DECLARE_FINAL_TYPE for BalsaCitebar. It is already opaque.

--- src/balsa-cite-bar.h
+++ src/balsa-cite-bar.h
@@ -12,7 +12,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
  * GNU General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,20 +26,16 @@
 G_BEGIN_DECLS
 
 
-#define BALSA_TYPE_CITE_BAR            (balsa_cite_bar_get_type())
-#define BALSA_CITE_BAR(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), BALSA_TYPE_CITE_BAR, BalsaCiteBar))
-#define BALSA_CITE_BAR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), BALSA_TYPE_CITE_BAR, BalsaCiteBarClass))
-#define BALSA_IS_CITE_BAR(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), BALSA_TYPE_CITE_BAR))
-#define BALSA_IS_CITE_BAR_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), BALSA_TYPE_CITE_BAR))
-#define BALSA_CITE_BAR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), BALSA_TYPE_CITE_BAR, BalsaCiteBarClass))
+#define BALSA_TYPE_CITE_BAR balsa_cite_bar_get_type()
 
+G_DECLARE_FINAL_TYPE(BalsaCiteBar,
+                     balsa_cite_bar,
+                     BALSA,
+                     CITE_BAR,
+                     GObject)
 
-typedef struct _BalsaCiteBar        BalsaCiteBar;
-typedef struct _BalsaCiteBarClass   BalsaCiteBarClass;
-
-GType      balsa_cite_bar_get_type   (void) G_GNUC_CONST;
-GtkWidget* balsa_cite_bar_new        (gint height, gint bars, gint dimension);
-void       balsa_cite_bar_resize     (BalsaCiteBar *cite_bar, gint height);
+GtkWidget* balsa_cite_bar_new    (gint height, gint bars, gint dimension);
+void       balsa_cite_bar_resize (BalsaCiteBar *cite_bar, gint height);
 
 
 G_END_DECLS

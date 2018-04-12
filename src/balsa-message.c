be0726c130ed073c3179175ba6579632d78662b6
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2018-04-11 23:32:44 -0400

    Fix the "scroll on click" issue
    Fixes https://bugzilla.gnome.org/show_bug.cgi?id=795085
    
    * src/balsa-message.c (balsa_message_init): wrap the
      BalsaMimeWidget::widget in a GtkViewport; if we do not,
      GtkContainer does it for us, but also sets it up to scroll when
      first clicked, which leaves the wrong text under the pointer.

--- src/balsa-message.c
+++ src/balsa-message.c
@@ -664,6 +664,7 @@ balsa_message_init(BalsaMessage * bm)
     GtkStack  *stack;
     GtkWidget *vbox;
     GtkWidget *scroll;
+    GtkWidget *viewport;
     GtkWidget **buttons;
     GtkTreeStore *model;
     GtkGesture *gesture;
@@ -705,7 +706,13 @@ balsa_message_init(BalsaMessage * bm)
     widget = balsa_mime_widget_get_widget(bm->bm_widget);
     g_signal_connect(widget, "notify::has-focus",
                      G_CALLBACK(balsa_mime_widget_check_focus), bm);
-    gtk_container_add(GTK_CONTAINER(bm->scroll), widget);
+
+    /* If we do not add the widget to a viewport, GtkContainer would
+     * provide one, but it would also set it up to scroll on grab-focus,
+     * which has been really annoying for a long time :-( */
+    viewport = gtk_viewport_new(NULL, NULL);
+    gtk_container_add(GTK_CONTAINER(viewport), widget);
+    gtk_container_add(GTK_CONTAINER(bm->scroll), viewport);
 
     /* structure view */
     model = gtk_tree_store_new (NUM_COLUMNS,

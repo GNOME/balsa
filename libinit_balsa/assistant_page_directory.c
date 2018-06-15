dadb8311d576e94df1bf9c3605cc9b2eafe7336e
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2018-06-15 15:36:41 -0400

    assistant_page_directory: adapt to new API
    * libinit_balsa/assistant_page_directory.c (unconditional_mailbox):
      adapt to new libbalsa_server_set_host() API.

--- libinit_balsa/assistant_page_directory.c
+++ libinit_balsa/assistant_page_directory.c
@@ -129,7 +129,9 @@ unconditional_mailbox(const gchar * path, const gchar * prettyname,
 
     if (is_remote) {
         libbalsa_server_set_host(LIBBALSA_MAILBOX_REMOTE_SERVER(*box),
-                                 url.host, ssl);
+                                 url.host,
+                                 ssl ? NET_CLIENT_CRYPT_ENCRYPTED :
+                                       NET_CLIENT_CRYPT_NONE);
         libbalsa_server_set_username(LIBBALSA_MAILBOX_REMOTE_SERVER(*box),
                                      getenv("USER"));
     }

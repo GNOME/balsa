ab31ab6127503015b76ff7af4a00c33b54595077
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2019-07-18 19:35:45 -0400

    balsa-cite-bar: Declare it final
    * src/balsa-cite-bar.c: Copyright update;
    * src/balsa-cite-bar.h: use G_DECLARE_FINAL_TYPE().

--- src/balsa-cite-bar.h
+++ src/balsa-cite-bar.h
@@ -1,6 +1,6 @@
 /* -*-mode:c; c-style:k&r; c-basic-offset:4; -*- */
 /* Balsa E-Mail Client
- * Copyright (C) 1997-2016 Stuart Parmenter and others
+ * Copyright (C) 1997-2019 Stuart Parmenter and others
  * Written by (C) Albrecht Dreﬂ <albrecht.dress@arcor.de> 2007
  *
  * This program is free software; you can redistribute it and/or modify
@@ -12,7 +12,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
  * GNU General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,20 +26,16 @@
 G_BEGIN_DECLS
 
 
-#define BALSA_TYPE_CITE_BAR            (balsa_cite_bar_get_type())
-#define BALSA_CITE_BAR(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), BALSA_TYPE_CITE_BAR, BalsaCiteBar))
-#define BALSA_CITE_BAR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), BALSA_TYPE_CITE_BAR, BalsaCiteBarClass))
-#define BALSA_IS_CITE_BAR(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), BALSA_TYPE_CITE_BAR))
-#define BALSA_IS_CITE_BAR_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), BALSA_TYPE_CITE_BAR))
-#define BALSA_CITE_BAR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), BALSA_TYPE_CITE_BAR, BalsaCiteBarClass))
+#define BALSA_TYPE_CITE_BAR balsa_cite_bar_get_type()
 
+G_DECLARE_FINAL_TYPE(BalsaCiteBar,
+                     balsa_cite_bar,
+                     BALSA,
+                     CITE_BAR,
+                     GtkWidget)
 
-typedef struct _BalsaCiteBar        BalsaCiteBar;
-typedef struct _BalsaCiteBarClass   BalsaCiteBarClass;
-
-GType      balsa_cite_bar_get_type   (void) G_GNUC_CONST;
-GtkWidget* balsa_cite_bar_new        (gint height, gint bars, gint dimension);
-void       balsa_cite_bar_resize     (BalsaCiteBar *cite_bar, gint height);
+GtkWidget* balsa_cite_bar_new    (gint height, gint bars, gint dimension);
+void       balsa_cite_bar_resize (BalsaCiteBar *cite_bar, gint height);
 
 
 G_END_DECLS

0b2819fb53671a26e16c809b720be7258d1945b6
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2019-11-25 18:32:54 -0500

    Do not unref GDateTime
    returned by g_mime_message_get_date(); we do not own the ref.

--- libbalsa/body.c
+++ libbalsa/body.c
@@ -119,10 +119,8 @@ libbalsa_message_body_extract_embedded_headers(GMimeMessage* msg)
 	ehdr->subject = g_strdup(_("(No subject)"));
 
     datetime = g_mime_message_get_date(msg);
-    if (datetime != NULL) {
+    if (datetime != NULL)
         ehdr->date = g_date_time_to_unix(datetime);
-        g_date_time_unref(datetime);
-    }
 
     return ehdr;
 }

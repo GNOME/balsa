04547d0a907234a568f2ad2c634e8a1edec52eed
Peter Bloomfield <PeterBloomfield@bellsouth.net>
2020-08-30 13:09:07 -0400

    Various: Rename libbalsa_add_button_to_box()
    to libbalsa_add_mnemonic_button_to_box(). Some of our buttons have
    images rather than text; the new name makes it clear why we don't use
    the helper in that case.

--- libbalsa/misc.h
+++ libbalsa/misc.h
@@ -163,7 +163,7 @@ gchar *libbalsa_font_string_to_css(const gchar * font_string, const gchar * name
 void libbalsa_parser_options_init(void);
 GMimeParserOptions *libbalsa_parser_options(void);
 
-GtkWidget * libbalsa_add_button_to_box(const gchar *markup,
+GtkWidget * libbalsa_add_mnemonic_button_to_box(const gchar *markup,
                                        GtkWidget   *box,
                                        GtkAlign     align);
 

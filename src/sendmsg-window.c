GNOME/balsa

commit 1612677d1cf1e3a7b2f5457e09f773579659b4e9

Fix bugzilla.redhat.com 1483354
Fix https://bugzilla.redhat.com/show_bug.cgi?id=1483354, balsa message window Queue toolbar button sends immediately

	* src/sendmsg-window.c: respect
	balsa_app.always_queue_sent_mail.

Signed-off-by: Peter Bloomfield <PeterBloomfield@bellsouth.net>

src/sendmsg-window.c
@@ -4156,7 +4156,7 @@ create_lang_menu(GtkWidget * parent, BalsaSendmsg * bsmsg)
 
 /* Standard buttons; "" means a separator. */
 static const BalsaToolbarEntry compose_toolbar[] = {
-    { "send",         BALSA_PIXMAP_SEND       },
+    { "toolbar-send", BALSA_PIXMAP_SEND       },
     { "", ""                                  },
     { "attach-file",  BALSA_PIXMAP_ATTACHMENT },
     { "", ""                                  },

commit ed1ee3b8362d2c60aae4d06bba027535401ff2e3

sendmsg-window: Send button sensitivity
Fix managing the sensitivity of the toolbar-send button,
broken by 1612677d1cf1e3a7b2f5457e09f773579659b4e9.

src/sendmsg-window.c
@@ -242,7 +242,7 @@ sw_preferred_charset(BalsaSendmsg * bsmsg)
  * lists of actions that are enabled or disabled as groups
  */
 static const gchar *const ready_actions[] = {
-    "send", "queue", "postpone"
+    "send", "toolbar-send", "queue", "postpone"
 };
 
 /* ===================================================================